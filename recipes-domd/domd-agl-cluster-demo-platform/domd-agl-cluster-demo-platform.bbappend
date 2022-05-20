xen-troops/meta-xt-prod-cockpit-demo

commit 93a97086da7b774fac125c78f95dd18d1a3e0721

domd: Use `https` to fetch kernel for all machines
We need to apply patch on top of `linux-renesas_4.14.bb` to replace
protocol `git` with `https` for all supported machines.

Fixes 932a7db5fb7097da141a32f3ceffe6e0183d9b03

Signed-off-by: Dmytro Semenets <dmytro_semenets@epam.com>
Acked-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>

recipes-domd/domd-agl-cluster-demo-platform/domd-agl-cluster-demo-platform.bbappend
@@ -25,6 +25,12 @@ XT_QUIRK_PATCH_SRC_URI_append_h3ulcb-4x2g-kf = "\
     file://0001-connman-main.conf-add-can-lo-vif-xenbr-eth-in-black-.patch;patchdir=meta-agl \
     file://0001-hdmi-a-1-180.cfg-change-transform-at-0.patch;patchdir=meta-agl-cluster-demo \
 "
+
+XT_QUIRK_PATCH_SRC_URI_append_salvator-xs-h3-4x2g = "\
+    file://0001-ctemplate-set-git-protocol-as-https.patch;patchdir=bsp/meta-rcar \
+    file://0001-linux-renesas-change-protocol-from-git-at-https.patch;patchdir=bsp/meta-renesas-rcar-gen3 \
+"
+
 # these layers will be added to bblayers.conf on do_configure
 XT_QUIRK_BB_ADD_LAYER += "meta-xt-prod-extra"
 XT_QUIRK_BB_ADD_LAYER += "meta-xt-agl-base"

commit ba8ed455c73c740bc9b74f5687e66899fb40c320

Fix "domd: Use https to fetch kernel for all machines"
Fixes: 93a97086da7b774fac125c78f95dd18d1a3e0721

Signed-off-by: Dmytro Semenets <dmytro_semenets@epam.com>
Reviewed-by: Ruslan Shymkevych <ruslan_shymkevych@epam.com>

recipes-domd/domd-agl-cluster-demo-platform/domd-agl-cluster-demo-platform.bbappend
@@ -20,14 +20,12 @@ SRC_URI_rcar_append = " \
 XT_QUIRK_PATCH_SRC_URI_append_h3ulcb-4x2g-kf = "\
     file://0001-linux-renesas-Remove-patch-230-from-renesas.scc.patch;patchdir=bsp/meta-rcar \
     file://0001-ctemplate-set-git-protocol-as-https.patch;patchdir=bsp/meta-rcar \
-    file://0001-linux-renesas-change-protocol-from-git-at-https.patch;patchdir=bsp/meta-renesas-rcar-gen3 \
     file://0001-kingfisher_output.cfg-remove-mode-off-for-HDMI-A-2.patch;patchdir=meta-agl \
     file://0001-connman-main.conf-add-can-lo-vif-xenbr-eth-in-black-.patch;patchdir=meta-agl \
     file://0001-hdmi-a-1-180.cfg-change-transform-at-0.patch;patchdir=meta-agl-cluster-demo \
 "
 
-XT_QUIRK_PATCH_SRC_URI_append_salvator-xs-h3-4x2g = "\
-    file://0001-ctemplate-set-git-protocol-as-https.patch;patchdir=bsp/meta-rcar \
+XT_QUIRK_PATCH_SRC_URI_append = "\
     file://0001-linux-renesas-change-protocol-from-git-at-https.patch;patchdir=bsp/meta-renesas-rcar-gen3 \
 "
 